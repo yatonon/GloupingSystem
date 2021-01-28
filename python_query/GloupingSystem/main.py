552a7ce022a2e6e101829ea0e8326694c301509c
toyoday <toyoda@localworks.jp>
2021-01-28 15:13:33 +0900

    plot 機能追加

--- python_query/GloupingSystem/main.py
+++ python_query/GloupingSystem/main.py
@@ -2,6 +2,9 @@
 import random
 import copy
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 import AccessDatabase.student as ADstudent
 from Methods.create import create_genom
 from Methods.select import select, next_generation_gene_create
@@ -18,6 +21,9 @@ SELECT_GENOM = 30
 GENOM_MUTATION = 0.1
 # 繰り返す世代数
 MAX_GENERATION = 100
+# pdf でデータのプロットをするか
+PDF_PLOT = True
+
 GetStudentDataInstance = ADstudent.GetStudentData()
 all_students = GetStudentDataInstance.get_student()
 all_student_nodes = GetStudentDataInstance.get_student_node()
@@ -30,7 +36,12 @@ if __name__ == '__main__':
         # 初期ゲノムの作成
         current_generation_individual_group.append(create_genom(all_students, TEAM_PEOPLE))
 
+    graph_left_list = []
+    graph_height_min_list = []
+    graph_height_max_list = []
+    graph_height_avg_list = []
     for count_ in range(1, MAX_GENERATION + 1):
+        graph_left_list.append(count_)
         # 現行世代個体集団の遺伝子を評価し、genomClassに代入します
         for i in range(MAX_GENOM_LIST):
             evaluation_result = evaluation(current_generation_individual_group[i], all_students, all_student_nodes)
@@ -49,6 +60,10 @@ if __name__ == '__main__':
         max_ = round(max(fits), 2)
         avg_ = round(sum(fits) / len(fits), 2)
 
+        graph_height_min_list.append(min_)
+        graph_height_max_list.append(max_)
+        graph_height_avg_list.append(avg_)
+
         # 現行世代の進化結果を出力します
         print("-----第{}世代の結果-----".format(count_))
         print("  Min:{}".format(min_))
@@ -59,6 +74,23 @@ if __name__ == '__main__':
         next_generation_individual_group = mutation(next_generation_individual_group, GENOM_MUTATION, TEAM_PEOPLE, len(all_students))
         # 現行世代と次世代を入れ替えます
         current_generation_individual_group = next_generation_individual_group
+
+    if PDF_PLOT:
+        # plot の pdf 化
+        graph_left = np.array(graph_left_list)
+        graph_height_min = np.array(graph_height_min_list)
+        graph_height_max = np.array(graph_height_max_list)
+        graph_height_avg = np.array(graph_height_avg_list)
+
+        plt.title("GA max, min, avg plot")
+        plt.xlabel("count")
+        plt.ylabel("score")
+        plt.grid(True)
+
+        plt.plot(graph_left, graph_height_min)
+        plt.plot(graph_left, graph_height_avg, color="yellow")
+        plt.plot(graph_left, graph_height_max, color="red")
+        plt.savefig("plot.pdf")
 
     # 最終結果出力
     print("最も優れた個体は{}".format(elite_genes[0].getGenom()))
